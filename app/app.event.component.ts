4e0d4acb811c8cb1472153eb912007bcf6d50c08
Nitin <206472558@TFAYD.com>
2016-12-12 16:39:54 +0000

    More html files updates

--- app/app.event.component.ts
+++ app/app.event.component.ts
@@ -6,9 +6,9 @@ import {Component}  from '@angular/core';
   template: `
     <div style="background-color:#E0B13A;;height:100px;"></div>
     <div style="background-color:#5336f3;">
-      <img src="images/events-main.jpg" style="width:100%;height:800px;">
+      <img src="images/events-main.jpg" style="width:100%;height:500px;">
     </div>
-<div style="padding:30px;width:66%;display:inline-block;">
+<div style="padding:30px;width:95%;display:inline-block;">
 <marquee>
     <h2>For latest UK event please visit this section</h2>
 </marquee>
